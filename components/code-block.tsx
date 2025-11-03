6a744e27477448e1c00bf69bc94fe54017a2acf5
rushabhcodes <mail@rushabh.dev>
2025-11-03 16:35:17 +0530

    style: format code and improve readability in globals.css and code-block.tsx

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -38,9 +38,13 @@ export function CodeBlock({
       {tab === "code" && (
         <pre
           {...props}
-          className={"text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900"}
+          className={
+            "text-sm w-full overflow-x-auto dark:bg-zinc-900 p-4 border border-zinc-200 dark:border-zinc-700 rounded-xl dark:text-zinc-50 text-zinc-900"
+          }
         >
-          <code className="whitespace-pre-wrap wrap-break-word">{children}</code>
+          <code className="whitespace-pre-wrap wrap-break-word">
+            {children}
+          </code>
         </pre>
       )}
 
